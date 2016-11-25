f59b8cb1a3c22314327fb7126f6b2e3e20b8028b
Дмитрий Поспелов <DPospelov@MacBook-Pro-Dmitrij.local>
2016-11-25 10:30:18 +0300

    new commit

--- ex1.py
+++ ex1.py
@@ -6,17 +6,11 @@ def draw(canvas):
     for sq in list1:
        canvas.draw_polygon([(sq[0], sq[1]), (sq[0] + 49, sq[1]), (sq[0] + 49, sq[1] + 49), (sq[0], sq[1] + 49)], 1, 'white', 'green')
 
-def ex(pos, list):
-    for sq in list:
-        if (pos[0] == sq[0] and pos[1] == sq[1]):
-            return True
-        else:
-            return False
 
 def click(pos):
     a = [pos[0] // 50 * 50, pos[1] // 50 * 50]
 
-    if ex(a, list1):
+    if a in list1:
         list1.pop(list1.index(a))
     else:
         list1.append(a)
